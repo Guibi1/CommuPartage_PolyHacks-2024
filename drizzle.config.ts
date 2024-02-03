a7872852809acdd6b898cc311fe7a6464bc288c0
Laurent Stéphenne <laurent@guibi.dev>
2024-02-03 15:46:08 -0500

    DRIZZLE WORKS DO NOT TOUCH

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,14 @@
+import { config } from "dotenv";
 import type { Config } from "drizzle-kit";
-import "dotenv";
+
+config();
+config({ path: ".env.local" });
 
 export default {
     schema: "./src/lib/db/schemas.ts",
-    dbCredentials: {
-        host:        process.env.DATABASE_URL as string,
-        database: "commupartage",
-        user:   process.env.DATABASE_USERNAME as string,
-        password: process.env.DATABASE_PASSWORD as string,
-    },
+    out: "./.drizzle",
     driver: "mysql2",
+    dbCredentials: {
+        uri: `mysql://${process.env.PLANETSCALE_DB_USERNAME}:${process.env.PLANETSCALE_DB_PASSWORD}@${process.env.PLANETSCALE_DB_HOST}/${process.env.PLANETSCALE_DB}?ssl={"rejectUnauthorized":true}`,
+    },
 } satisfies Config;
