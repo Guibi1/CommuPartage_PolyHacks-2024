ecd07084b872e324110572acb2fd0a815177cff0
Gabriel Mikus <113815444+mikusgabriel@users.noreply.github.com>
2024-02-04 02:23:01 -0500

    Maison a laurent

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,16 +21,11 @@
         const { AdvancedMarkerElement, PinElement } = await loader.importLibrary("marker");
 
         // Create an array of alphabetical characters used to label the markers.
-        const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-        const infoWindow = new InfoWindow({
-            content: "",
-            disableAutoPan: true,
-        });
+        const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789abcdefghijklmnopqrstuvwxyz";
 
         const markers = locations.map((position, i) => {
             const label = labels[i % labels.length];
-            const pinGlyph =new  PinElement({
+            const pinGlyph = new PinElement({
                 glyph: label,
                 glyphColor: "white",
             });
@@ -41,46 +36,23 @@
 
             // markers can only be keyboard focusable when they have click listeners
             // open info window when marker is clicked
-            marker.addListener("click", () => {
-                infoWindow.setContent(position.lat + ", " + position.lng);
-                infoWindow.open(map, marker);
-            });
+            marker.addListener("click", () => {});
             return marker;
         });
         // Add a marker clusterer to manage the markers.
 
         let map = new Map(mapDiv, {
-            zoom: 3,
-            center: { lat: -28.024, lng: 140.887 },
-            mapId: 'DEMO_MAP_ID',
+            zoom: 14,
+            center: { lat: 45.5048442, lng: -73.6184641 },
+            mapId: "DEMO_MAP_ID",
         });
         new MarkerClusterer({ markers, map });
     });
 
     const locations = [
-        { lat: -31.56391, lng: 147.154312 },
-        { lat: -33.718234, lng: 150.363181 },
-        { lat: -33.727111, lng: 150.371124 },
-        { lat: -33.848588, lng: 151.209834 },
-        { lat: -33.851702, lng: 151.216968 },
-        { lat: -34.671264, lng: 150.863657 },
-        { lat: -35.304724, lng: 148.662905 },
-        { lat: -36.817685, lng: 175.699196 },
-        { lat: -36.828611, lng: 175.790222 },
-        { lat: -37.75, lng: 145.116667 },
-        { lat: -37.759859, lng: 145.128708 },
-        { lat: -37.765015, lng: 145.133858 },
-        { lat: -37.770104, lng: 145.143299 },
-        { lat: -37.7737, lng: 145.145187 },
-        { lat: -37.774785, lng: 145.137978 },
-        { lat: -37.819616, lng: 144.968119 },
-        { lat: -38.330766, lng: 144.695692 },
-        { lat: -39.927193, lng: 175.053218 },
-        { lat: -41.330162, lng: 174.865694 },
-        { lat: -42.734358, lng: 147.439506 },
-        { lat: -42.734358, lng: 147.501315 },
-        { lat: -42.735258, lng: 147.438 },
-        { lat: -43.999792, lng: 170.463352 },
+        { lat: 45.5225691, lng: -73.7620294 },
+        { lat: 45.5527626, lng: -73.6658292 },
+        { lat: 45.5762141, lng: -73.6579424 },
     ];
 </script>
 
